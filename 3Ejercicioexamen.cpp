6b865a35d404bfe1bf7120848c199fb95f612f78
Alexander <achicallag@unjbg.edu.pe>
2024-06-14 12:49:18 -0500

    Ejercicio 3 resuelto, sin funciones

--- 3Ejercicioexamen.cpp
+++ 3Ejercicioexamen.cpp
@@ -1,7 +1,25 @@
 /*
 3 Ejercicio
-Cree un programa para voltear el numero ingresado
+Cree un programa para invertir el numero ingresado, con funciones
 ejemplo
 1002 -> 2001
 */
 
+#include <iostream>
+using namespace std;
+
+int main(){
+	int a,s,x;
+	cout << "Ingrese un numero que desea invertir" << endl;
+	cin >> x;
+	s=0;
+	 while (x>0){
+	 		a = x % 10;
+	 	s = (s * 10) + a;
+	 	x = x / 10;
+	
+	 }
+	cout << "El numero invertido sera: " << s;
+		
+	return 0;
+}
